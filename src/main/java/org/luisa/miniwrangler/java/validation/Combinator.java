a21d1dcb34226d5d42672b9b9ec17b18e41f51e9
Luisa Pinto <xtremelu@gmail.com>
2019-08-15 23:11:31 +0100

    Update javadoc

--- src/main/java/org/luisa/miniwrangler/java/validation/Combinator.java
+++ src/main/java/org/luisa/miniwrangler/java/validation/Combinator.java
@@ -2,10 +2,30 @@ package org.luisa.miniwrangler.java.validation;
 
 import java.util.Collection;
 
+/**
+ * Combinator or errors or successes
+ *
+ * @author developer
+ *
+ * @param <E> this type should represent an error
+ * @param <S> this type should represent a success
+ */
 public interface Combinator<E, S> {
 
-	String combineErrors(Collection<E> errors);
+    /**
+     * Combines a collection of errors
+     *
+     * @param errors the given collection
+     * @return a String a textual representation of combined errors
+     */
+    String combineErrors(Collection<E> errors);
 
-	String combineSuccesses(Collection<S> successes);
+    /**
+     * Combines a collection of successes
+     *
+     * @param successes the given collection
+     * @return a String a textual representation of combined successes
+     */
+    String combineSuccesses(Collection<S> successes);
 
 }
